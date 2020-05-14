0d60457a0b0909295cfcec29f54ecae25a127234
Samuel Shaw <shaw.samuelj@outlook.com>
2020-05-14 14:03:09 -0400

    Working solution

---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StringCalculator
+{
+    public interface IStringCalculator
+    {
+        public int Add(string numbers);
+    }
+
+    public class StringCalculator : IStringCalculator
+    {
+        private const string DelimiterDefiner = "//";
+        private readonly IList<string> _delimiters = new List<string> {",", "\n" };
+
+        public int Add(string numbers)
+        {
+            string cleanedNumbersString = numbers;
+            if (numbers.StartsWith(DelimiterDefiner))
+            {
+                cleanedNumbersString = cleanedNumbersString.Remove(0, DelimiterDefiner.Length);
+
+                string newDelimiter;
+                if (cleanedNumbersString.StartsWith("\n\n"))
+                    newDelimiter = "\n";
+                else
+                {
+                    int indexOfFirstNewLine = cleanedNumbersString.IndexOf("\n", StringComparison.Ordinal);
+                    newDelimiter = cleanedNumbersString.Substring(0, indexOfFirstNewLine);
+                }
+
+                int indexOfCommaDelimiter = _delimiters.IndexOf(",");
+                _delimiters.RemoveAt(indexOfCommaDelimiter);
+                _delimiters.Add(newDelimiter);
+
+                cleanedNumbersString = cleanedNumbersString.Remove(0, newDelimiter.Length + 1);
+            }
+
+
+            if (string.IsNullOrEmpty(cleanedNumbersString))
+                return 0;
+
+            var delimitersAsCharArray = _delimiters.Select(d => d.First()).ToArray();
+            var numbersAsStringsList = cleanedNumbersString.Split(delimitersAsCharArray);
+
+            return numbersAsStringsList.Select(int.Parse).Sum();
+        }
+    }
+}
+;
---
+++ StringCalculator/StringCalculatorTests.cs
@@ -0,0 +1,71 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace StringCalculator
+{
+    public class StringCalculatorTests
+    {
+        private readonly IStringCalculator _stringCalculator;
+
+        public StringCalculatorTests()
+        {
+            _stringCalculator = new StringCalculator();
+        }
+
+        [Test]
+        public void AddMethodReturnsZeroForEmptyString()
+        {
+            int result = _stringCalculator.Add("");
+
+            result.Should().Be(0);
+        }
+
+        [Test]
+        public void AddMethodReturnsValueOfNumber()
+        {
+            int result = _stringCalculator.Add("7");
+
+            result.Should().Be(7);
+        }
+
+        [Test]
+        public void AddMethodReturnsValueOfSummedNumber()
+        {
+            int result = _stringCalculator.Add("7,1");
+
+            result.Should().Be(8);
+        }
+
+        [Test]
+        public void AddMethodReturnsSumOfUnknownAmountOfNumbers()
+        {
+            int result = _stringCalculator.Add("5,48,5,18,1,5,7");
+
+            result.Should().Be(89);
+        }
+
+        [Test]
+        public void AddMethodReturnsSumOfNumbersWithNewLineDelimiter()
+        {
+            int result = _stringCalculator.Add("7\n1");
+
+            result.Should().Be(8);
+        }
+
+        [Test]
+        public void AddMethodReturnsSumOfNumbersWithNewLineOrCommaDelimiter()
+        {
+            int result = _stringCalculator.Add("1,7\n1,8");
+
+            result.Should().Be(17);
+        }
+
+        [Test]
+        public void AddMethodShouldDeclareDelimiter()
+        {
+            int result = _stringCalculator.Add("//;\n1;2");
+
+            result.Should().Be(3);
+        }
+    }
+}
